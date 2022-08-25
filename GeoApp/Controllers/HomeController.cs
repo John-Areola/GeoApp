941a6874ba256c6bad9dd468785fd92084354757
Areola John <areolajohnbayo@outlook.com>
2022-08-25 23:53:57 +0100

    Add project files.

---
+++ GeoApp/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using GeoApp.Models.DB_Model;
+using GeoApp.Models.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+        public ActionResult Register(Customers newcustomer)
+        {
+            try
+            {
+                var db = new Model1();
+                var cust = new Customers();
+                cust.firstName = newcustomer.firstName;
+                cust.lastName = newcustomer.lastName;
+                cust.DOB = newcustomer.DOB;
+                db.SaveChanges();
+            return View();
+            }
+            catch (Exception Ex)
+            {
+                ViewBag.ErrorMessage = "An Error Occured during Registration";
+                return View();
+            }
+        }
+        
+    }
+}
---
+++ GeoApp/Models/View Model/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeoApp.Models.View_Model
+{
+    public class Customers
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public DateTime DOB { get; set; }
+    }
+}
